83aa3f54d3a931f4f45a0fe2957a378e95156687
Cayennechia19 <cayennechia@gmail.com>
2025-02-08 07:58:10 +0000

    Pending changes exported from your codespace

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "No babiii 😞...", 
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
-    "I will be very very very sad...",
+    "I will be very very very sakit hati💔...",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
